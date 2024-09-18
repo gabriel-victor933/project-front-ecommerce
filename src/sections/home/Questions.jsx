6d68f6505afd6cb8ba74da7ecc87745af82c84c6
gabriel-victor933 <gabrielvictor933@usp.br>
2024-09-18 08:54:36 -0300

    feat: add filtering by category of questions

--- src/sections/home/Questions.jsx
+++ src/sections/home/Questions.jsx
@@ -7,56 +7,120 @@ const questions = [
     {
         question: 'Can I modify my order after placing it?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'ordering'
     },
     {
         question: 'How do I initiate a return?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'shipping'
     },
     {
         question: 'How can I unsubscribe from the newsletter?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'ordering'
     },
     {
         question: 'Do you offer exchanges for products?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'shipping'
     },
     {
         question: 'How can I place an order on Klothink?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'ordering'
     },
     {
         question: 'What payment methods do you accept?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'ordering'
     },
     {
         question: 'How can I track my order?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'shipping'
     },
     {
         question: 'What is your shipping policy?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'shipping'
     },
     {
         question: 'Are there any additional fees for returns?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'customer support'
     },
     {
         question: 'How do I create an account on Klothink?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'customer support'
     },
     {
         question: 'Can I change my account information?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'customer support'
     },
     {
         question: 'Are my personal details secure on Klothink?',
         answer: 'Ordering is easy! Simply browse our website, add items to your cart, and proceed to checkout. Follow the prompts to enter your details and complete your purchase.',
+        category: 'customer support'
     },
 ]
+
+// eslint-disable-next-line react/prop-types
+function QuestionContainer({ children }){
+    return (
+        <Grid2
+        sx={{
+            display: 'grid',
+            gridTemplateColumns: '1fr',
+            rowGap: {xl: '20px', md: '16px', xs: '16px'},
+        }}
+    >
+        {children}
+        </Grid2> 
+    )
+}
 
 export default function Questions(){
 
     const [category, setCategory] = useState('ALL')
+
+    function filterAndDivideQuestions(category){
+        const arr = [[],[],[]]
+
+        let  filteredQuestions = [...questions]
+
+        if(category != 'ALL'){
+            
+            filteredQuestions = filteredQuestions.filter((question) => {
+                return question.category === category.toLowerCase()})
+        }
+
+        for(let i = 0; i < filteredQuestions.length; i++){
+            let res = i%3
+            arr[res].push(filteredQuestions[i])
+        }
+
+        return arr
+    }
+
+    function renderQuestions(category){
+        if(!category) return 
+        const arr = filterAndDivideQuestions(category)
+
+        const containers = []
+
+        for(let selected of arr){
+            containers.push(
+                <QuestionContainer>
+                    {selected.map((question, i) => <QuestionsCard key={i} {...question}/>)}
+                </QuestionContainer>
+            )
+        }
+
+
+        return containers
+    }
 
 
     return (
@@ -117,33 +181,7 @@ export default function Questions(){
                     alignItems: 'start'
                 }}
             >
-                <Grid2
-                    sx={{
-                        display: 'grid',
-                        gridTemplateColumns: '1fr',
-                        rowGap: {xl: '20px', md: '16px', xs: '16px'},
-                    }}
-                >
-                    {questions.slice(0,4).map((question, i) => <QuestionsCard key={i} {...question}/>)}
-                </Grid2>    
-                <Grid2
-                    sx={{
-                        display: 'grid',
-                        gridTemplateColumns: '1fr',
-                        rowGap: {xl: '20px', md: '16px', xs: '16px'},
-                    }}
-                >
-                    {questions.slice(0,4).map((question, i) => <QuestionsCard key={i} {...question}/>)}
-                </Grid2>  
-                <Grid2
-                    sx={{
-                        display: 'grid',
-                        gridTemplateColumns: '1fr',
-                        rowGap: {xl: '20px', md: '16px', xs: '16px'},
-                    }}
-                >
-                    {questions.slice(0,4).map((question, i) => <QuestionsCard key={i} {...question}/>)}
-                </Grid2>  
+                {renderQuestions(category)}
             </Grid2>
         </Grid2>
     )
